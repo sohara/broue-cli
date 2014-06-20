bdb72b8b83bad6976fd308c5994b1e14c6632b35
Sean O'Hara <sohara@sohara.com>
2014-06-20 11:22:04 -0400

    Remove console logging

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
   headers: function() {
-    console.log("In headers function");
     return {
       'auth-token': JSON.parse(localStorage.getItem('user')).token,
       'auth-email': JSON.parse(localStorage.getItem('user')).email
--- app/routes/brews/index.js
+++ app/routes/brews/index.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    console.log("In brews index model hook");
     return this.modelFor('brews');
   }
 });
