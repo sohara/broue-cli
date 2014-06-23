0d20ad7010e9f5a1ba3499552eb95bfcfb7ae5d2
Sean O'Hara <sohara@sohara.com>
2014-06-23 15:36:13 -0400

    Get bootstrap glypicon fonts working with broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -20,4 +20,15 @@ app.import({development:'vendor/route-recognizer/dist/route-recognizer.js'});
 app.import({development:'vendor/FakeXMLHttpRequest/fake_xml_http_request.js'});
 app.import({development:'vendor/pretender/pretender.js'});
 
-module.exports = app.toTree();
+app.import('vendor/bootstrap/dist/js/bootstrap.js');
+app.import('vendor/bootstrap/dist/css/bootstrap.css');
+
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
+var extraAssets = pickFiles('vendor/bootstrap/dist/fonts',{
+    srcDir: '/',
+    files: ['**/*'],
+    destDir: '/fonts'
+});
+
+module.exports = mergeTrees([app.toTree(), extraAssets]);
