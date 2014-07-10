d1eceb9cf314c5389c5f5db111c61ef6dc34cbd9
Sean O'Hara <sohara@sohara.com>
2014-07-10 10:43:33 -0400

    Ensure that logging out clears out all Ember Data from store

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,20 +4,31 @@ export default Ember.Controller.extend({
   needs: ['login'],
   session: Ember.computed.alias('controllers.login.session'),
   user: function(key, value) {
-   if (arguments.length > 1) {
-     return value;
-   }
-   if ( this.get('session') !== null && typeof(this.get('session')) != 'undefined') {
-     return this.store.find('user', this.get('session.user_id'));
-   }
+    if (arguments.length > 1) {
+      return value;
+    }
+    if ( this.get('session') !== null && typeof(this.get('session')) != 'undefined') {
+      return this.store.find('user', this.get('session.user_id'));
+    }
   }.property('session'),
+
+  clearStore: function() {
+    var typeMaps = this.store.get('typeMaps');
+    for(var typeId in typeMaps) {
+      if(typeMaps.hasOwnProperty(typeId)) {
+        var modelName = typeMaps[typeId].type.toString().split(":")[1];
+        this.store.unloadAll(modelName);
+      }
+    }
+  },
 
   actions: {
     logout: function() {
-      this.setProperties({
-        session: null,
-        user: null});
+      this.setProperties({ session: null, user: null });
       this.transitionToRoute('login');
+      Ember.run.next(this, function() {
+        this.clearStore();
+      });
     }
   }
 });
