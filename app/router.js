4dd9fec47014f6a27b4342355b9d0b8dac08d602
Sean O'Hara <sohara@sohara.com>
2014-07-21 11:29:52 -0400

    Integrate google analytics

--- app/router.js
+++ app/router.js
@@ -46,4 +46,11 @@ Router.map(function() {
   });
 });
 
-export default Router;
+export default Router.reopen({
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+      'page': this.get('url'),
+           'title': this.get('url')
+    });
+  }.on('didTransition')
+});;
