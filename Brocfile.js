6038bc896119d0a1fe23cf25eafc3fc5a3258008
Sean O'Hara <sohara@sohara.com>
2014-07-21 10:07:39 -0400

    Use grunt for client-only deploys via s3, cloudfront, redistogo

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,15 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  fingerprint: {
+    prepend: 'https://dbwwdwzhcqlk3.cloudfront.net/'
+  },
+  minifyCSS: {
+    enabled: true,
+    options: {}
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
