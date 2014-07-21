7721586a70cbcb5739b2ef48b9a3190d49de8d15
Sean O'Hara <sohara@sohara.com>
2014-07-21 10:40:16 -0400

    Use api/v1 namespace for ember data adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
+  namespace: 'api/v1',
   headers: function() {
     var session = this.login.get('session');
     if (session !== null) {
