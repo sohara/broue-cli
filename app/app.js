3009255daee63651c3744fe4f026466386f10c79
Sean O'Hara <sohara@sohara.com>
2014-09-01 17:17:39 -0400

    Don't reassign Router to Ember.Route - use special jshint config comment instead

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
+/* jshint unused: false */
+
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import Route from './ext/route-reopen';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
-Ember.Route = Route;
 
 var App = Ember.Application.extend({
   modulePrefix: 'broue-cli', // TODO: loaded via config
