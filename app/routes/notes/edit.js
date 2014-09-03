639ca33b3869abb63d3a288394c7f004cff53e84
Sean O'Hara <sohara@sohara.com>
2014-09-02 22:37:50 -0400

    Ensure blank notes are not persisted. Closes #49 - Use removeObject to remove from brew's notes collection - Check for blank text attrs before saving

--- app/routes/notes/edit.js
+++ app/routes/notes/edit.js
@@ -5,24 +5,34 @@ export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('note', params.resource_id);
   },
+
   deactivate: function() {
     var model = this.controllerFor('notes/edit').get('model');
     if (model.get('isNew')) {
+      var brew = this.modelFor('brew');
       model.deleteRecord();
+      brew.get('notes').removeObject(model);
     } else {
       model.rollback();
     }
   },
+
   actions: {
     save: function(model) {
       var _this = this;
-      model.save().then(function() {
-        _this.transitionTo('notes.index');
-      });
-      // Need to add catch/or promise reject handling code here
-      // .catch(function(reason) {
-      // });
+      if (model.get('text.length') > 0) {
+
+        model.save().then(function() {
+          _this.transitionTo('notes.index');
+        });
+        // Need to add catch/or promise reject handling code here
+        // .catch(function(reason) {
+        // });
+      } else {
+        this.transitionTo('notes.index');
+      }
     },
+
     cancel: function() {
       this.transitionTo('notes.index');
     }
