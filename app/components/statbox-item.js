d92c49a8d3937b0b8b0eee054f3a37617c001082
Sean O'Hara <sohara@sohara.com>
2019-03-27 22:48:03 -0400

    Migrate recipe view to use new table components

--- app/components/statbox-item.js
+++ app/components/statbox-item.js
@@ -1,9 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ["slate-statbox"],
-  classNameBindings: ['colorClass', 'gridClass'],
-
-  gridClass: 'col-md-3',
+  tagName: '',
   faIcon: 'fa-flask'
 });
